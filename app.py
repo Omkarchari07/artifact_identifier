3cd8e8af9a95c11588397ceff0654603564e0f65
Omkar Chari <omkarchari0707@gmail.com>
2026-07-10 13:15:13 +0530

    removed multiple predict request

--- app.py
+++ app.py
@@ -168,8 +168,6 @@ def predict():
     app.logger.info("Image preprocessing completed in %.2f seconds", time.perf_counter() - preprocess_start)
 
     predict_start = time.perf_counter()
-    preds = model.predict(img)[0]
-    app.logger.info("model.predict() completed in %.2f seconds", time.perf_counter() - predict_start)
 
     app.logger.info("Starting TensorFlow inference...")
 
@@ -178,7 +176,7 @@ def predict():
     app.logger.info(
         "TensorFlow inference completed in %.2f seconds",
         time.perf_counter() - predict_start
-    )
+)
 
     top_idx = int(np.argmax(preds))
 
